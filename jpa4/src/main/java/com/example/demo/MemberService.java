50e4b92e812976772171865870e7af7480f73dca
Rysdne <nemo3882@gmail.com>
2022-09-23 11:51:43 +0900

    22_0923_01

--- jpa4/src/main/java/com/example/demo/MemberService.java
+++ jpa4/src/main/java/com/example/demo/MemberService.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -31,8 +33,11 @@ public class MemberService {
 	}
 
 	public MemberEntity save(MemberEntity member) {
-		
 		return repo.save(member);
+	}
+
+	public Page<MemberEntity> findAll(Pageable pageable) {
+		return repo.findAll(pageable);
 	}
 	
 }
